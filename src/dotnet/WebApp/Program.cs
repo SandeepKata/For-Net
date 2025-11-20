2c74da9be043ed1a5d3b2bfc1ddc25fa6f33d9cc
SandeepKata <109897634+SandeepKata@users.noreply.github.com>
2025-11-20 15:38:52 +0530

    Update Program.cs

--- src/dotnet/WebApp/Program.cs
+++ src/dotnet/WebApp/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from .NET Web App!!");
+app.MapGet("/", () => "Hello from .NET Web App!");
 
 app.Run();
+
